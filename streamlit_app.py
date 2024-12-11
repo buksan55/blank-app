6007d4ae61491cb11e6cde508291bb134f179576
buksan55 <143041698+buksan55@users.noreply.github.com>
2024-12-10 18:48:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     The name of the dataset is "Police_arrests". I found this dataset from data.illinois.gov. Here is the link of this dataset. https://data.illinois.gov/dataset/police-arrests/resource/ca1dceb3-01f8-4a56-935b-7e3035ff60a4.
 This dataset is available on the Data.illinois.gov. However, the dataset's page does not specify a particular license or terms of use. In the absense of explicit licensing information, I assume that the data is intended for public access and use, as it is hosted on a government open data platform.
 This usually allows for activites such as downloading, analyzing, and sharing the data, provided that proper attribution is given and the data is not used for cmmercial purposes without permission. The Police_arrests' file size is approximaetly 39.4 MB. It contains 206,600 rows and 25 columns. Github's web interface support file uploads up to 100 MB. So, I can directly upload file to a GIthub repository without issues.)."
-)
+
